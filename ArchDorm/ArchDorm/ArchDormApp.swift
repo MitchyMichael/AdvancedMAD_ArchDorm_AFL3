8ff7af2771dd4c9caf8c3c834f9b08abcea04b90
Michael Wijaya Sutrisna <84000679+MitchyMichael@users.noreply.github.com>
2022-06-06 08:45:43 +0700

    Added Splash Screen

---
+++ ArchDorm/ArchDorm/ArchDormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchDormApp.swift
+//  ArchDorm
+//
+//  Created by SIFT - Telkom DBT Air 5 on 06/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ArchDormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreen()
+        }
+    }
+}
